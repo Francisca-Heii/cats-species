f441e714fb7e40485469b2699301b36fe39fab3b
Francisca-Heii <franciscahheii@gmail.com>
2023-06-07 20:14:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {getCatsFetch} from './catState';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const cats = useSelector(state => state.cats.cats);
+  const Loading = useSelector(state => state.cats.isLoading);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getCatsFetch());
+  }, [dispatch] );
+  console.log(cats);
+  // console.log();
+  console.log(Loading);
+
+  {
+    if (Loading){
+      return (
+        <h1>Loading the Data!</h1>
+      );
+    }
+    else{
+      return (
+        <div className="App">
+          <h1>Cats Species Gallery</h1>
+          <hr/>
+          <div className='gallery'>
+            { cats.map(cat =>
+            <div key={cat.id} className='row'>
+              <div className='column column-left'>
+                  <h2>{cat.name}</h2>
+              </div>
+              <div className='column column-right'>
+                <h5>Temprement:{cat.temprement}</h5>
+                  <h2>{cat.description}</h2>
+              </div> 
+              </div>    
+            )}
+
+          </div>
     </div>
-  );
+
+      );
+    }
+  }
+
 }
 
 export default App;
